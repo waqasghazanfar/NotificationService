749bb9be244f61f60e8090f8c9a3ec7271a1956f
waqasghazanfar <ghazanfar.waqas@gmail.com>
2025-06-28 17:19:35 +0100

    Add project files.

---
+++ NotificationService.Api/Authentication/SecuritySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace NotificationService.Api.Authentication
+{
+    public class SecuritySettings
+    {
+        public bool EnableAuthentication { get; set; } = false;
+        public string ClientId { get; set; } = string.Empty;
+        public string ClientSecret { get; set; } = string.Empty;
+    }
+}
